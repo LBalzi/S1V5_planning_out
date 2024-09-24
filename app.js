946b230d1451441e55e0f85aa2e77f96f7436435
Lorenzo Balzi <lorenzo.balzi@outlook.com>
2024-09-24 16:51:36 +0200

    commit from work

--- app.js
+++ app.js
@@ -2,19 +2,42 @@ const express = require('express');
 const app = express();
 const records = require('./records');
 
+app.use(express.json());
+
 //Send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', async (req, res)=>{
+  try {
   const quotes = await records.getQuotes();
   res.json(quotes);
+  } catch(err) {
+    res.json({message: err.message})
+  }
 });
 // Send a GET request to /quotes/:id to READ a quote
 app.get('/quotes/:id', async (req, res)=>{
+ try {
   const quote = await records.getQuote(req.params.id);
   res.json(quote);
-
+ } catch(err) {
+  res.json({message: err.message})
+ }
 });
 
 // Send a POST request to /quotes to CREATE a new quote
+app.post('/quotes', async (req, res)=>{
+  try{
+    const quote = await records.createQuote({
+      quote: req.body.quote,
+      author: req.body.author 
+    });
+    res.json(quote);
+  } catch(err){
+    res.status(500);
+    res.json({message: err.message})
+  }
+
+});
+
 // Send a PUT request to /quotes/:id to UPDATE a quote
 // Send a DELETE request to /quotes/:id to delete a quote
 // Send a GET request to READ a random quote
